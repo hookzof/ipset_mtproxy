7c181389259c39ae5a93e538cd2249d3ec06b89f
Vitaly Kryukov <admin@friezy.ru>
2019-09-03 14:27:12 +0300

    Add files via upload

--- sources/install.go
+++ sources/install.go
@@ -6,22 +6,13 @@ import (
 	"os/exec"
 )
 
-func cmd(cmd string, shell bool) []byte {
-	if shell {
-		out, err := exec.Command("bash", "-c", cmd).Output()
-		if err != nil {
-			log.Println(err)
-		}
-
-		return out
-	}
-
-	out, err := exec.Command(cmd).Output()
+func cmd(cmd string) string {
+	out, err := exec.Command("sh", "-c", cmd).Output()
 	if err != nil {
 		log.Println(err)
 	}
 
-	return out
+	return string(out)
 }
 
 func main() {
@@ -30,34 +21,47 @@ func main() {
 	first := flag.Bool("badhosts", false, "a bool")
 	second := flag.Bool("digitalocean", false, "a bool")
 	third := flag.Bool("countryblock", false, "a bool")
+	fourth := flag.Bool("rugov", false, "a bool")
 
 	flag.Parse()
 
 	log.Println("Dependency check...")
-	cmd("apt -y install git unzip ipset", true)
+	cmd("apt -y install git unzip ipset")
+
+	log.Println("[backup] ipset (/etc/backup.ipset.up.rules)")
+	cmd("ipset save > /etc/backup.ipset.up.rules")
+	log.Println("[backup] iptables (/etc/backup.rules.v4)")
+	cmd("iptables-save > /etc/backup.rules.v4")
 
 	log.Println("Downloading and extracting...")
-	cmd("cd /opt && git clone https://github.com/hookzof/ipset_mtproxy && cd ipset_mtproxy && unzip ipset.up.zip", true)
-	cmd("ipset save > /etc/backup.ipset.up.rules && ipset destroy && ipset restore < /opt/ipset_mtproxy/ipset.up.rules", true)
+	cmd("cd /opt && git clone https://github.com/hookzof/ipset_mtproxy && cd ipset_mtproxy && unzip ipset.up.zip")
+
+	log.Println("An attempt to destroy ipset and restore rules...")
+	cmd("ipset destroy && ipset restore < /opt/ipset_mtproxy/ipset.up.rules")
 
 	if *first {
-		cmd("iptables -A INPUT -m set --match-set badhosts src -j DROP", true)
+		cmd("iptables -A INPUT -m set --match-set badhosts src -j DROP")
 		log.Println("[iptables] badhosts added")
 	}
 
 	if *second {
-		cmd("iptables -A INPUT -m set --match-set digitalocean src -j DROP", true)
+		cmd("iptables -A INPUT -m set --match-set digitalocean src -j DROP")
 		log.Println("[iptables] digitalocean added")
 	}
 
 	if *third {
-		cmd("iptables -A INPUT -m set --match-set countryblock src -j DROP", true)
+		cmd("iptables -A INPUT -m set --match-set countryblock src -j DROP")
 		log.Println("[iptables] countryblock added")
 	}
 
-	if *first || *second || *third {
-		log.Println("[iptables] Saving rules... [/etc/rules.v4]")
-		cmd("iptables-save > /etc/rules.v4", true)
+	if *fourth {
+		cmd("iptables -A INPUT -m set --match-set rugov src -j DROP")
+		log.Println("[iptables] rugov added")
+	}
+
+	if *first || *second || *third || *fourth {
+		log.Println("[iptables] Saving rules... (/etc/rules.v4)")
+		cmd("iptables-save > /etc/rules.v4")
 	} else {
 		log.Println("[iptables] Rules have not been added, check startup keys")
 	}
