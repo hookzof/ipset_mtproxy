13ec460de56cc922dc0a4199308f1ff8b17c924e
Vitaly Kryukov <admin@friezy.ru>
2019-09-09 11:24:44 +0300

    more friendly for ipset

--- sources/install.go
+++ sources/install.go
@@ -9,7 +9,7 @@ import (
 func cmd(cmd string) string {
 	out, err := exec.Command("sh", "-c", cmd).Output()
 	if err != nil {
-		log.Println(err)
+		log.Println(err, " ("+cmd+")")
 	}
 
 	return string(out)
@@ -17,6 +17,8 @@ func cmd(cmd string) string {
 
 func main() {
 	log.Println("Starting!")
+
+	/* Flag parse */
 
 	first := flag.Bool("badhosts", false, "a bool")
 	second := flag.Bool("digitalocean", false, "a bool")
@@ -34,43 +36,36 @@ func main() {
 	cmd("iptables-save > /etc/backup.rules.v4")
 	log.Println("[backup] iptables (/etc/backup.rules.v4)")
 
+	/* White list */
+
 	if cmd("iptables-save | grep \"67.207.74.182\"") == "" {
 		cmd("iptables -I INPUT -s 67.207.74.182 -j ACCEPT -m comment --comment \"https://test.ton.org addr\"")
+	}
+
+	if cmd("iptables-save | grep \"138.68.76.208\"") == "" {
+		cmd("iptables -I INPUT -s 138.68.76.208 -j ACCEPT -m comment --comment \"ipset_mtproxy white list\"")
 	}
 
 	log.Println("[system] Downloading and extracting...")
 	cmd("cd /opt && git clone https://github.com/hookzof/ipset_mtproxy && cd ipset_mtproxy && unzip ipset.up.zip")
 
-	log.Println("[iptables] Checking and deleting past iptables rules for correct operation...")
+	log.Println("[iptables] Checking and deleting past iptables/ipset rules for correct operation...")
 
 	if cmd("iptables-save | grep \"badhosts src\"") != "" {
 		cmd("iptables -D INPUT -m set --match-set badhosts src -j DROP")
+		cmd("ipset destroy badhosts")
 	}
-
-	if cmd("iptables-save | grep \"digitalocean src\"") != "" {
-		cmd("iptables -D INPUT -m set --match-set digitalocean src -j DROP")
-	}
-
-	if cmd("iptables-save | grep \"countryblock src\"") != "" {
-		cmd("iptables -D INPUT -m set --match-set countryblock src -j DROP")
-	}
-
-	if cmd("iptables-save | grep \"rugov src\"") != "" {
-		cmd("iptables -D INPUT -m set --match-set rugov src -j DROP")
-	}
-
-	if cmd("iptables-save | grep \"mikrotik src\"") != "" {
-		cmd("iptables -D INPUT -m set --match-set mikrotik src -j DROP")
-	}
-
-	log.Println("[ipset] An attempt to destroy ipset and restore rules...")
-	cmd("ipset destroy")
 
 	if *first {
 		cmd("ipset restore < /opt/ipset_mtproxy/badhosts")
 		log.Println("[ipset] badhosts added")
 		cmd("iptables -A INPUT -m set --match-set badhosts src -j DROP")
 		log.Println("[iptables] badhosts added")
+	}
+
+	if cmd("iptables-save | grep \"digitalocean src\"") != "" {
+		cmd("iptables -D INPUT -m set --match-set digitalocean src -j DROP")
+		cmd("ipset destroy digitalocean")
 	}
 
 	if *second {
@@ -80,6 +75,11 @@ func main() {
 		log.Println("[iptables] digitalocean added")
 	}
 
+	if cmd("iptables-save | grep \"countryblock src\"") != "" {
+		cmd("iptables -D INPUT -m set --match-set countryblock src -j DROP")
+		cmd("ipset destroy countryblock")
+	}
+
 	if *third {
 		cmd("ipset restore < /opt/ipset_mtproxy/countryblock")
 		log.Println("[ipset] countryblock added")
@@ -87,11 +87,21 @@ func main() {
 		log.Println("[iptables] countryblock added")
 	}
 
+	if cmd("iptables-save | grep \"rugov src\"") != "" {
+		cmd("iptables -D INPUT -m set --match-set rugov src -j DROP")
+		cmd("ipset destroy rugov")
+	}
+
 	if *fourth {
 		cmd("ipset restore < /opt/ipset_mtproxy/rugov")
 		log.Println("[ipset] rugov added")
 		cmd("iptables -A INPUT -m set --match-set rugov src -j DROP")
 		log.Println("[iptables] rugov added")
+	}
+
+	if cmd("iptables-save | grep \"mikrotik src\"") != "" {
+		cmd("iptables -D INPUT -m set --match-set mikrotik src -j DROP")
+		cmd("ipset destroy mikrotik")
 	}
 
 	if *fifth {
